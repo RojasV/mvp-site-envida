9ad7429b7fdfcd6647d337834ae6c1868b3ec6eb
Vladimir Andre Rojas <vrojas@FAZENDA.MS>
2023-05-25 10:59:10 -0300

    adicionando funcionalidade de envio de email com alerta de resposta positiva

--- public/utils/mailSender.js
+++ public/utils/mailSender.js
@@ -1,46 +1,97 @@
 class MailSender {
     constructor() {
-        this.mailEl = document.querySelector('.php-email-form')
-        this.submitBtn = document.querySelector('.mail-submit')
         this.sendMail()
-
     }
 
-    async sendMail() {
-        // this.mailEl.addEventListener('submit', e => {
-        //     e.preventDefault()
+    sendMail() {
+        //  this.form.addEventListener('submit', e => {
+        //      e.preventDefault()
 
-        //     console.log('calling')
+        //      console.log('calling')
 
-        //     // this.submitBtn.setAttribute("disabled", true)
-        //     // this.spinnerEl.style.display = "block"
-        //     // this.formContainerEl.classList.add("opacity")
+        //     //   this.submitBtn.setAttribute("disabled", true)
+        //     //   this.spinnerEl.style.display = "block"
+        //     //   this.formContainerEl.classList.add("opacity")
 
-        //     // const formData = new FormData(this.mailEl)
+        //       const formData = new FormData(this.form)
 
-        //     // let cellphoneHandledToStringNumber = "55" + formData.get("celphone").replace("(", "").replace(")", "").replace(" ", "").replace("-", "")
+        //       let cellphoneHandledToStringNumber = "55" + formData.get("cellphone").replace("(", "").replace(")", "").replace(" ", "").replace("-", "")
 
-        //     // const templateParams = {
-        //     //     from_name: formData.get("name"),
-        //     //     email: formData.get("email"),
-        //     //     celphone: cellphoneHandledToStringNumber
-        //     // }
+        //       const templateParams = {
+        //           from_name: formData.get("name"),
+        //           email: formData.get("email"),
+        //           celphone: cellphoneHandledToStringNumber
+        //       }
 
-        //     // emailjs.send('service_0lhss64', "template_4ndxcal", templateParams, 'gQaGWN1YCHugVnOdV')
-        //     //     .then(() => {
-        //     //         this.successAlertHandler(templateParams.from_name)
-        //     //         this.submitBtn.setAttribute("disabled", false)
-        //     //         this.mailEl.reset()
-        //     //     })
-        //     //     .catch(err => {
-        //     //         console.log(err)
-        //     //     })
-        // })
+        //       emailjs.send('service_i886f7l', "template_o70ftqt", templateParams, 'hyMMZ8mvv0Jzyp0UL')
+        //           .then(() => {
+        //               this.successAlertHandler(templateParams.from_name)
+        //               this.submitBtn.setAttribute("disabled", false)
+        //               this.form.reset()
+        //           })
+        //           .catch(err => {
+        //               console.log(err)
+        //           })
+        //  })
 
-        $(document).on('submit', '.php-email-form', function (event) {
+        $(document).on('submit', '.php-email-form', '.mail-submit', async function (event) {
             event.preventDefault()
 
-            console.log("calling")
+            const submitBtn = document.querySelector('.mail-submit')
+            const form = document.querySelector('.php-email-form')
+            const formData = new FormData(form)
+
+            submitBtn.setAttribute("disabled", true)
+
+            const cellphoneHandledToStringNumber = "55" + formData.get("cellphone").replace("(", "").replace(")", "").replace(" ", "").replace("-", "")
+
+            const templateParams = {
+                name: formData.get("name"),
+                email: formData.get("email"),
+                age: formData.get("age"),
+                cellphone: cellphoneHandledToStringNumber
+            }
+
+            await emailjs.send('service_i886f7l', "template_o70ftqt", templateParams, 'hyMMZ8mvv0Jzyp0UL')
+            await Swal.fire({
+                title: `Ficamos felizes pelo seu interesse ${templateParams.name}!`,
+                text: 'Recebemos o seu email e te responderemos em no máximo 1 minutinho!',
+                icon: 'success',
+                confirmButtonText: "ok"
+            })
+
+            form.reset()
+            submitBtn.removeAttribute("disabled")
+        })
+
+        $(document).on('submit', '.php-email-form-modal', '.mail-submit', async function (event) {
+            event.preventDefault()
+
+            const submitBtn = document.querySelector('.mail-submit')
+            const form = document.querySelector('.php-email-form-modal')
+            const formData = new FormData(form)
+
+            submitBtn.setAttribute("disabled", true)
+
+            const cellphoneHandledToStringNumber = "55" + formData.get("cellphone").replace("(", "").replace(")", "").replace(" ", "").replace("-", "")
+
+            const templateParams = {
+                name: formData.get("name"),
+                email: formData.get("email"),
+                age: formData.get("age"),
+                cellphone: cellphoneHandledToStringNumber
+            }
+
+            await emailjs.send('service_i886f7l', "template_o70ftqt", templateParams, 'hyMMZ8mvv0Jzyp0UL')
+            await Swal.fire({
+                title: `Ficamos felizes pelo seu interesse ${templateParams.name}!`,
+                text: 'Recebemos o seu email e te responderemos em no máximo 1 minutinho!',
+                icon: 'success',
+                confirmButtonText: "ok"
+            })
+
+            form.reset()
+            submitBtn.removeAttribute("disabled")
         })
     }
 
@@ -54,7 +105,7 @@ class MailSender {
             confirmButtonText: "ok"
         })
             .then(() => {
-                this.submitBtn.removeAttribute("disabled")
+                // this.submitBtn.removeAttribute("disabled")
             })
             .catch(err => {
                 console.log(err)
